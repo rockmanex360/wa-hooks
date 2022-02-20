db06c31d0cc9494e1fa2682fe8958a2deea437a5
R. Irvan Nur Firdaus <irvan.nurf21@gmail.com>
2022-02-20 20:09:08 +0700

    initial project

---
+++ WAHooks/Controllers/WhatsAppController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WAHooks.DTOs;
+
+namespace WAHooks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhatsAppController : ControllerBase
+    {
+        public WhatsAppController()
+        {}
+
+        [HttpGet("webhooks")]
+        public WebHookRequest Authentication([FromQuery] WebHookRequest request)
+        {
+            return request;
+        }
+    }
+}
---
+++ WAHooks/DTOs/WebHookRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WAHooks.DTOs
+{
+    public class WebHookRequest
+    {
+        [FromQuery(Name = "hub.mode")]
+        public string Mode { get; set; }
+        [FromQuery(Name = "hub.challenge")]
+        public int Challenge { get; set; }
+        [FromQuery(Name = "hub.verify_token")]
+        public string VerifyToken { get; set; }
+    }
+}
